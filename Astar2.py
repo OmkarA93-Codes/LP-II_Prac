874a26f68d1e719a918cffacd41636658eb463cc
OmkarA93-Codes <omkarandhare493@gmail.com>
2026-05-12 22:34:41 +0530

    Astar updated

--- Astar2.py
+++ Astar2.py
@@ -23,21 +23,13 @@ def astar(graph, heuristic, start, goal):
     while open_list:
 
         # Node with lowest priority
-        current_priority, current = heapq.heappop(open_list)
+         current_cost, current = heapq.heappop(open_list)
 
-        # Skip if already visited
-        if current in closed_list:
-            continue
-
-        # Add node to closed list
-        closed_list.add(current)
-
-        # Goal reached
-        if current == goal:
+         if current == goal:
 
             path = []
 
-            while current is not None:
+            while current:
                 path.append(current)
                 current = parent[current]
 
@@ -45,34 +37,21 @@ def astar(graph, heuristic, start, goal):
 
             return path, cost[goal]
 
-        # Explore neighbors
-        for neighbour, weight in graph[current]:
+         for neighbour, weight in graph[current]:
 
-            # Ignore visited nodes
-            if neighbour in closed_list:
-                continue
-
-            # g(n)
-            new_cost = cost[current] + weight
-
-            # Better path found
-            if neighbour not in cost or new_cost < cost[neighbour]:
+             new_cost = cost[current] + weight
+ 
+             if neighbour not in cost or new_cost < cost[neighbour]:
 
                 cost[neighbour] = new_cost
 
-                # f(n) = g(n) + h(n)
                 priority = new_cost + heuristic[neighbour]
 
-                # Add to open list
                 heapq.heappush(open_list, (priority, neighbour))
 
-                # Store parent
                 parent[neighbour] = current
 
-    return None, None
 
-
-# Graph
 graph = {
     'A': [('B', 1), ('C', 3)],
     'B': [('D', 1)],
@@ -80,13 +59,14 @@ graph = {
     'D': []
 }
 
-# Heuristic values
 heuristic = {
     'A': 3,
     'B': 2,
     'C': 1,
     'D': 0
 }
+
+path, total_cost = astar(graph, heuristic, 'A', 'D')
 
 # Run A*
 path, total_cost = astar(graph, heuristic, 'A', 'D')
